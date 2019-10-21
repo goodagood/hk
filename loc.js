7e16873742b3e8ac9cbfc8003896f04e37a50543
Andrew <bin1510@gmail.com>
2019-10-21 13:11:28 +0800

    2019 1021 1311pm

---
+++ loc.js
@@ -0,0 +1,10 @@
+
+const popoStations = {
+    monkok : {lat:22.324612 , lng: 114.168564 },
+    hungHom : {lat:22.315374 , lng: 114.177405 },
+    kowloonCity : {lat:22.324438 , lng: 114.184954 },
+    wKowloonHQ : {lat:22.323530 , lng: 114.183486 },
+    hq : {lat:22.278610 , lng: 114.168641 },
+    central : {lat:22.288970 , lng: 114.147959 },
+    yauMaTei : {lat:22.310313 , lng: 114.165064 }
+};
