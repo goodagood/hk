c2b59c5ff064c2ee1eb1f144ac3ef195da9f9135
Andrew <bin1510@gmail.com>
2019-10-29 21:30:08 +0800

    2019 1029

--- app.js
+++ app.js
@@ -14,15 +14,14 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "statics")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
-// [START hello_world]
 // Say hello!
 app.get('/', (req, res) => {
     console.log(req.headers);
     console.log(JSON.stringify(req.headers));
     res.status(200).send('Hello, world! popomap');
 });
-// [END hello_world]
 
 
 app.get('/map', (req, res) => {
@@ -34,8 +33,8 @@ app.get('/hk', (req, res) => {
 app.post('/hk', (req, res) => {
 
     console.log(req.body);
-    console.log(req.headers);
-    console.log("going to response with a json.");
+    //console.log(req.headers);
+    //console.log("going to response with a json.");
 
     res.writeHead(200, {"Content-Type": "application/json"});
 
@@ -47,14 +46,29 @@ app.post('/hk', (req, res) => {
 });
 
 
+// socket.io
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+    console.log('io on connection');
+
+    socket.on('talk', function (data) {
+        console.log('talk: ', data); // {pos: {lat: , lng:}, message: }
+        socket.broadcast.emit('talk', data); 
+    });
+
+});
+
+
 if (module === require.main) {
-  // [START server]
   // Start the server
-  const server = app.listen(process.env.PORT || 8080, () => {
-    const port = server.address().port;
+    // please NOTE: server.listen INSTEAD OF app.listen for socket.io
+    const appsvr = server.listen(process.env.PORT || 8080, () => {
+    const port = appsvr.address().port;
     console.log(`App listening on port ${port}`);
   });
-  // [END server]
 }
 
-module.exports = app;
+
+
+module.exports = server;
