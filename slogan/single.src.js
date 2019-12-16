0c97262e9ebfb4deb4c5240d953b5a4f66938a73
andrew <bin1510@gmail.com>
2019-12-17 05:34:44 +0800

    2019 1217 0534

--- slogan/single.src.js
+++ slogan/single.src.js
@@ -1,4 +1,6 @@
 
+const $ = require("jquery");
+
 const pcolor = require("./color.js");
 
 const p = console.log;
@@ -21,32 +23,59 @@ document.getElementById('textColor').onclick = function(e){
  * Do body font size
  */
 
+var Obj = {baseSize : 0};
+
+function getBaseSize(){
+    if(Obj.baseSize < 5){
+        Obj.baseSize = getBodyFontSize();
+        return Obj.baseSize;
+    }else{
+        return Obj.baseSize;
+    }
+}
+
 function getBodyFontSize(){
-  var fs = document.body.style.fontSize;
-  if (fs && fs !== ""){
-    return fs;
-  }
-  return window.getComputedStyle(document.body).fontSize;
+    var fs = document.body.style.fontSize;
+    if (fs && fs !== ""){
+        ;
+        //
+    }else{
+        fs = window.getComputedStyle(document.body).fontSize;
+    }
+    var intSize  = parseInt(fs);
+    return intSize;
 }
 
 
 function increaseBodyFontSize(e){
-  e.preventDefault();
-  var size = getBodyFontSize();
-  intSize  = parseInt(size);
-  //p(`size: ${size}, `);
-  big = intSize + 1;
-  //p(`size + 1: ${big}, `);
-  document.body.style.fontSize = `${big}px`;
+    if(e) {e.preventDefault();}
+
+    var size = getBaseSize();
+
+    Obj.baseSize = size + 1;
+    big = size + 1;
+    bigPx = `${big}px`;
+    //p(`size + 1: ${big}, `);
+    //document.body.style.fontSize = `${big}px`;
+
+    var paragraphs = document.getElementsByTagName('p');
+    //p(`paragraphs len: ${paragraphs.length}`);
+    $('p').each(function(index, pa){
+        pa.style.lineHeight = `${big + big * 0.2}px`;
+        pa.style.fontSize   = bigPx;
+    });
 }
 function decreaseBodyFontSize(e){
-  e.preventDefault();
-  var size = getBodyFontSize();
-  intSize  = parseInt(size);
-  //p(`size: ${size}, `);
-  small = intSize - 1;
-  //p(`size - 1: ${small}, `);
-  document.body.style.fontSize = `${small}px`;
+    //if(e) {e.preventDefault();}
+    var size = getBaseSize();
+    Obj.baseSize = size - 1;
+    small = size - 1;
+
+    var paragraphs = document.getElementsByTagName('p');
+    $('p').each(function(index, pa){
+        pa.style.lineHeight = `${small + small * 0.2}px`;
+        pa.style.fontSize   = `${small}px`;
+    });
 }
 
 var increaseEl = document.getElementById("increaseBodyFontSize");
@@ -55,6 +84,17 @@ if (increaseEl.addEventListener){
 }else if (increaseEl.attachEvent){
     increaseEl.attachEvent('onclick', increaseBodyFontSize);
 }
+
+$("#increaseBodyFontSize").mousedown(function(e){
+    var id = setInterval(function(){
+        increaseBodyFontSize();
+    }, 50);
+
+    $(this).mouseup(function(){
+        p('mouse up');
+        clearInterval(id);
+    });
+});
 
 var decreaseEl = document.getElementById("decreaseBodyFontSize");
 if (decreaseEl.addEventListener){
@@ -69,13 +109,13 @@ if (decreaseEl.addEventListener){
 
 
 document.getElementById('yes').onclick = function(e){
-    if(e) e.preventDefault();
-
-    var textel = document.getElementById('textInput');
+    if(e) e.preventDefault(); var textel = document.getElementById('textInput');
     const text = textel.value || " ";
     p(textel.value);
 
     var paragraph = document.createElement("p");
+    //paragraph.className += " row"; // for bootstrap css
+
     var textnode = document.createTextNode(text);
     paragraph.appendChild(textnode);
 
